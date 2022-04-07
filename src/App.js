4cab1dcf471f90edea77e27d03aa43b201a9f93c
Jalpesh Ahir <jalpeshahir6512@gmail.com>
2022-04-07 16:21:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [resourceType, setResourceType] = useState("posts");
+
+  useEffect(() => {
+    console.log("resource changed");
+
+    // clear last entry
+    return () => {
+      console.log("return from resource change");
+    };
+  }, [resourceType]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <button onClick={() => setResourceType("posts")}>posts</button>
+      <button onClick={() => setResourceType("users")}>user</button>
+      <button onClick={() => setResourceType("comments")}>comments</button>
+      <h1>{resourceType}</h1>
+    </main>
   );
 }
 
